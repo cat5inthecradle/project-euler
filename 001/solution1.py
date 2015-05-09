e530fa5e9adfbad9c3aa94002ebe11d8f4b5eaee
Darin Webb <darinlwebb@gmail.com>
2015-05-08 19:59:39 -0500

    Completed 001

--- 001/solution1.py
+++ 001/solution1.py
@@ -4,12 +4,12 @@ i=1
 s=0
 
 while i < 1000:
-	if i%3==0:
-		s+=i
-	elif i%5==0:
+	if i%3==0 or i%5==0:
 		s+=i
 	i+=1
 
-print("Finished\n--------\nSUM:",s)
+print("Finished")
+print("--------")
+print("SUM:",s)
 
 
